bb321d5cb58cf8f37ac1e96d461af892e44baf1d
Orit Samuel <oritsamuelseid@gmail.com>
2024-11-02 21:58:48 +0300

    python

--- Introduction_to_Django/LibraryProject/bookshelf/admin.py
+++ Introduction_to_Django/LibraryProject/bookshelf/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Book
+
+admin.site.register(Book)
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author', 'publication_year')
+    
+    list_filter = ('author', 'publication_year')
+    search_fields = ('title', 'author')
---
+++ Introduction_to_Django/LibraryProject/bookshelf/admin_setup.md
@@ -0,0 +1,3 @@
+ defines a BookAdmin class that inherits from admin.ModelAdmin.
+The list_display attribute specifies a tuple of field names that will be shown in the list view.
+
