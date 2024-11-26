a4a83e7a8fafaeb9a328e5f6711d1e5b3dcb580e
Orit Samuel <oritsamuelseid@gmail.com>
2024-11-26 05:23:29 +0300

    endpoints

---
+++ api_project/api/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import BookList
+
+
+urlpatterns = [
+    path('books/', BookList.as_view()),  # Maps to the BookList view
+]
--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework.generics import ListAPIView
+from .serializers import BookSerializer
+from .models import Book
+
+class BookList(ListAPIView): 
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
