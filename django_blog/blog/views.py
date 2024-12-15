a1cad370cb3863b0a836d521433b65565b2e0676
Orit Samuel <oritsamuelseid@gmail.com>
2024-12-15 19:04:48 +0300

    p2

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import RegistrationForm
+from django.shortcuts import render, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import get_user_model
 
-# Create your views here.
+User = get_user_model()
+def register(request):
+  if request.method == 'POST':
+    form = RegistrationForm(request.POST)
+    if form.is_valid():
+      form.save()
+      return redirect('login')  # Redirect to login page after successful registration
+  else:
+    form = RegistrationForm()
+  return render(request, 'registration.html', {'form': form})
+
+@login_required  # Restrict access to logged-in users
+def profile(request):
+  user = get_object_or_404(User, pk=request.user.pk)
+  # ... Handle user details and update logic
+  return render(request, 'profile.html', {'user': user})
--- django_blog/django_blog/urls.py
+++ django_blog/django_blog/urls.py
@@ -16,7 +16,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.contrib.auth import views as auth_views
+from . import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('register/', views.register, name='register'),
+    path('profile/', views.profile, name='profile'),
 ]
