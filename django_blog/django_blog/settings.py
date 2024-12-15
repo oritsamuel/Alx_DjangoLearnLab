ea0dc1a13ada475041ee6b5fb5b22ed07b6babb6
Orit Samuel <oritsamuelseid@gmail.com>
2024-12-15 18:44:38 +0300

    set up

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 import os
 
+USER = os.getenv('USER', 'default_user')
+PORT = os.getenv('PORT', 8000)
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
