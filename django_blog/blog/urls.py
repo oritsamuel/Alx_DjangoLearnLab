00808d36353a11be7a668be16d95a4e4f21cfe00
orit <oritsamuelseid@gmail.com>
2025-01-09 12:10:54 -0800

    p2

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -8,9 +8,21 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(template_name='blog/logout.html'), name='logout'),
     path('register/', views.register, name='register'),
     path('profile/', views.profile, name='profile'),
+        
+    # List of all blog posts
     path('', views.PostListView.as_view(), name='post_list'),
-    path('posts/<int:pk>/', views.PostDetailView.as_view(), name='post_detail'),
-    path('posts/new/', views.PostCreateView.as_view(), name='post_create'),
-    path('posts/<int:pk>/edit/', views.PostUpdateView.as_view(), name='post_edit'),
-    path('posts/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post_delete'),
+    
+    # View for a single post
+    path('post/<int:pk>/', views.PostDetailView.as_view(), name='post_detail'),
+    
+    # Create a new post (Only for authenticated users)
+    path('post/new/', views.PostCreateView.as_view(), name='post_create'),
+    
+    # Edit an existing post (Only for the post author)
+    path('post/<int:pk>/edit/', views.PostUpdateView.as_view(), name='post_edit'),
+    
+    # Delete a post (Only for the post author)
+    path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post_delete'),
 ]
+
+
